192630fce3eeff0ab264e158f4e1239e222d20f1
Hayley Kwok <wingk1226@gmail.com>
2021-11-26 22:20:58 +0000

    Add project files.

---
+++ Decrypt.Model/Facebook/SharedAdvertiser.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+//path: ads_information/advertisers_using_your_activity_or_information.json
+namespace Decrypt.Model.Facebook
+{
+    public class AdvertisersUsingYourActivity
+    {
+        [JsonPropertyName("custom_audiences_all_types_v2")]
+        public IEnumerable<SharedAdvertiser>? SharedAdvertisers { get; set; }
+    }
+
+    public class SharedAdvertiser
+	{
+        [JsonPropertyName("advertiser_name")]
+		public string? Name { get; set; }
+
+		[JsonPropertyName("has_data_file_custom_audience")]
+        public bool HasDataFileCustomAudience { get; set; }
+
+        [JsonPropertyName("has_remarketing_custom_audience")]
+        public bool HasRemarketingCustomAudience { get; set; }
+
+        [JsonPropertyName("has_in_person_store_visit")]
+        public bool HasInPersonStoreVisit { get; set; }
+	}
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.IO.Compression;
+using Decrypt.Model.Facebook;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var Options = new JsonSerializerOptions();
+
+await using var stream = new FileStream(@"C:\Users\wingk\hayley\uni\cs_year3_Sem1\diss\data\fb-json\ads_information\advertisers_using_your_activity_or_information.json", FileMode.Open);
+// var results = await JsonSerializer.DeserializeAsync<AdvertisersUsingYourActivity>(stream, Options);
+//
+// foreach (var result in results.SharedAdvertisers)
+// {
+//     Console.WriteLine(result.Name);
+// }
+
+var zipPath = @"C:\Users\wingk\hayley\uni\cs_year3_Sem1\diss\data\fb-json\ads_information.zip";
+using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+{
+    foreach (ZipArchiveEntry entry in archive.Entries)
+    {
+        if (entry.FullName.EndsWith("advertisers_using_your_activity_or_information.json", StringComparison.OrdinalIgnoreCase))
+        {
+            var unzippedEntryStream = entry.Open();
+            var results = await JsonSerializer.DeserializeAsync<AdvertisersUsingYourActivity>(unzippedEntryStream, Options);
+        }
+    }
+}
