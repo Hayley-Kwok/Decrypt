73851a03bfc2f3b460b9f808f7c1997785b30d83
Hayley Kwok <wingk1226@gmail.com>
2021-11-28 21:02:03 +0000

    fix the facebook decoding issue with non english characters

---
+++ Decrypt.Model/FBConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace Decrypt.Model
+{
+    public class FbConverter : JsonConverter<string>
+    {
+        public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var latin1 = Encoding.GetEncoding("ISO-8859-1");
+
+            var bytes = reader.ValueSpan.ToArray();
+            var str = Encoding.UTF8.GetString(bytes);
+
+            var unescapedStr = Regex.Unescape(str);
+            return Encoding.UTF8.GetString(latin1.GetBytes(unescapedStr));
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value);
+        }
+    }
+}
