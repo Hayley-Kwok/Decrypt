231ec6fc0e01a0d0fc440c0d549736767c7c9ff2
Hayley Kwok <wingk1226@gmail.com>
2022-02-18 13:15:01 +0000

    Added the card with line chart (currently working with hard coded values)

--- Decrypt/Program.cs
+++ Decrypt/Program.cs
@@ -3,6 +3,10 @@ using Decrypt.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-GB");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-GB");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
