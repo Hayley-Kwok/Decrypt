4fc69318600ba83736e117c66fc41b9297f5c947
Hayley Kwok <wingk1226@gmail.com>
2022-04-17 20:20:43 +0100

    Add on functionality to update & change dashboard

--- Privasight.Wasm/Services/DataServiceDashboard.cs
+++ Privasight.Wasm/Services/DataServiceDashboard.cs
@@ -4,40 +4,65 @@ namespace Privasight.Wasm.Services
 {
     public partial class DataService
     {
-        private List<DashboardSetting>? _fbDashboardSettings;
+        public enum UpdateAction
+        {
+            Add,
+            Update,
+            Delete
+        }
 
-        public List<DashboardSetting>? FbDashboardSettings
+        private HashSet<DashboardSetting>? _fbDashboardSettings;
+
+        public HashSet<DashboardSetting>? FbDashboardSettings
         {
             get => _fbDashboardSettings;
             private set
             {
-                if(_fbDashboardSettings!= null && _fbDashboardSettings.Equals(value)) return;
+                if (_fbDashboardSettings != null && _fbDashboardSettings.Equals(value)) return;
                 _fbDashboardSettings = value;
                 OnPropertyChanged();
             }
         }
 
-        public async Task UpdateFbDashboardSettings(IEnumerable<DashboardSetting> newSettings)
+        public async Task UpdateFbDashboardSettings(DashboardSetting newSetting, UpdateAction action,
+            DashboardSetting? oldSetting = null)
         {
             if (FbDashboardSettings == null)
             {
                 await SetFbDashboardsFromStorage();
             }
 
-            FbDashboardSettings!.AddRange(newSettings);
+            switch (action)
+            {
+                case UpdateAction.Add:
+                    FbDashboardSettings!.Add(newSetting);
+                    break;
+                case UpdateAction.Update:
+                    FbDashboardSettings!.Add(newSetting);
+                    if (oldSetting != null) FbDashboardSettings!.Remove(oldSetting);
+                    break;
+                case UpdateAction.Delete:
+                    FbDashboardSettings!.Remove(newSetting);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(action), action, null);
+            }
+
+            OnPropertyChanged(nameof(FbDashboardSettings));
             await _localStorage.SetItemAsync(nameof(FbDashboardSettings), FbDashboardSettings);
         }
 
         public async Task SetFbDashboardsFromStorage()
         {
-            var dashboardSettings = await _localStorage.GetItemAsync<List<DashboardSetting>>(nameof(FbDashboardSettings));
-            FbDashboardSettings = dashboardSettings ?? new List<DashboardSetting>();
+            var dashboardSettings =
+                await _localStorage.GetItemAsync<HashSet<DashboardSetting>>(nameof(FbDashboardSettings));
+            FbDashboardSettings = dashboardSettings ?? new HashSet<DashboardSetting>();
         }
 
-        public async Task RemoveFbDashboardSettings()
+        public async Task ClearFbDashboardSettings()
         {
             await _localStorage.RemoveItemAsync(nameof(FbDashboardSettings));
-            FbDashboardSettings = new List<DashboardSetting>();
+            FbDashboardSettings = new HashSet<DashboardSetting>();
         }
     }
 }
