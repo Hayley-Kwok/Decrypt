f79e83fb09dbfec24578317fd1b33712d39f1895
Hayley Kwok <wingk1226@gmail.com>
2022-05-01 14:51:59 +0100

    Remove BlazoredLocalStorage dependencies

--- Privasight.Wasm/Program.cs
+++ Privasight.Wasm/Program.cs
@@ -4,10 +4,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Privasight.Wasm.Services;
 using Radzen;
 using System.Globalization;
-using Blazored.LocalStorage;
-using Blazored.LocalStorage.Serialization;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Privasight.Wasm.Data;
 
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-GB");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-GB");
@@ -21,7 +17,5 @@ builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<CompanyDataService>();
 builder.Services.AddScoped<DashboardService>();
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.Replace(ServiceDescriptor.Scoped<IJsonSerializer, NewtonSoftJsonSerializer>());
 
 await builder.Build().RunAsync();
