b74e28d7f913e6624661f124ce06dd6c79f4c7ae
Hayley-Kwok <wingk1226@gmail.com>
2023-04-28 11:40:16 +0000

    Deploying to gh-page from  @ b30e3c77727c5bf213ca5c0e092c39e84588b315 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
-const CACHE_VERSION = '4825562720'
+const CACHE_VERSION = '4830179812'
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
